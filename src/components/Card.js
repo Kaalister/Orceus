d7863f6f92f891c5c388a02e038ee0433800b18a
Kaalister <samuel.caillot@epitech.eu>
2020-11-29 19:29:20 +0100

    adding fullcard

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,19 +1,78 @@
 import React from 'react';
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import {  
+    ArrowBack,
+    ZoomIn,
+    ZoomOut,
+    ZoomOutMap,
+} from '@material-ui/icons';
+
+import data from '../Getcardbyid';
+import "../assets/css/card.css";
+import { Link } from 'react-router-dom';
+
 export default class CardMenu extends React.Component {
 
+    //this.props.id => id of cards
     constructor(props) {
         super(props);
 
-        this.state={
+        this.state = {
+            card: '',
+        }
+    }
 
-        };
+    componentDidMount() {
+        let card = data();
+
+        this.setState({
+            card
+        });
     }
 
     render() {
-        console.log(this.props.id)
         return (
-            <div>
-                Card id : {this.props.id}
+            <div className="center">
+                <TransformWrapper
+                    defaultScale={1}
+                    >
+                    {({ zoomIn, zoomOut, resetTransform, ...rest }) => (
+                        <React.Fragment>
+                            <Link
+                                className="tool-btn tool-back-arrow"
+                                to="/cards">
+                                <ArrowBack/>
+                            </Link>
+                            <div
+                                className="tool-btn tool-zoomin"
+                                onClick={zoomIn}
+                            >
+                                <ZoomIn/>
+                            </div>
+                            <div
+                                className="tool-btn tool-zoomout"
+                                onClick={zoomOut}
+                            >
+                                <ZoomOut/>
+                            </div>
+                            <div
+                                className="tool-btn tool-reset"
+                                onClick={resetTransform}
+                            >
+                                <ZoomOutMap/>
+                            </div>
+                            <TransformComponent>
+                                <div className="mx-auto">
+                                <img
+                                    className="full-card"
+                                    src={"data:image/png;base64," + this.state.card.img}
+                                    alt=" "
+                                    />
+                                </div>
+                            </TransformComponent>
+                        </React.Fragment>
+                    )}
+                </TransformWrapper>
             </div>
         );
     }
